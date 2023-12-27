bc4133ffeedd128ad093aac3c9891db8e5afeec5
Satyam Saurabh <satyam.saurabh@app-scoop.com>
2023-12-28 01:49:05 +0530

    small fix

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,9 +1,18 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { Navigate, useLocation } from "react-router-dom";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Navigate, useLocation } from 'react-router-dom';
+import { AuthState } from '../reducers/authSlice';
 
-const PrivateRoute = ({ children }) => {
-  const { isAuthenticated } = useSelector((state) => ({
+interface IAuthState {
+  auth: AuthState;
+}
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const PrivateRoute: React.FC<Props> = ({ children }) => {
+  const { isAuthenticated } = useSelector((state: IAuthState) => ({
     isAuthenticated: state?.auth?.isAuthenticated,
   }));
   const location = useLocation();
